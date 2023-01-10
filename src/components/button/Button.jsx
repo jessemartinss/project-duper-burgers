27f88743ac2a53844e33b64f343d91b02e46ed4c
Jessé Martins <jessemartins.ti@gmail.com>
2023-01-10 10:50:42 -0300

    finalization of button components

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import './Button.scss';
+
+export function ButtonOrangeSmall(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-orange-small">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOrangeDefault(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-orange-default">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOrangeLarge(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-orange-large">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonWhiteSmall(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-white-small">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonWhiteDefault(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-white-default">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonWhiteLarge(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-white-large">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineWhiteSmall(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-white-small">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineWhiteDefault(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-white-default">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineWhiteLarge(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-white-large">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineOrangeSmall(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-orange-small">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineOrangeDefault(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-orange-default">{props.value}</button>
+    </React.Fragment>
+  )
+}
+
+export function ButtonOutlineOrangeLarge(props) {
+  return (
+    <React.Fragment>
+      <button className="db-btn-outline-orange-large">{props.value}</button>
+    </React.Fragment>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Home.scss';
-import ImgHome01 from '../../assets/images/img-home-01.jpg';
 import HomeSection01 from './HomeSection01';
 
 export default function Home() {
